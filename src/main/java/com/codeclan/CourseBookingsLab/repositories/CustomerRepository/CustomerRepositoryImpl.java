4414ee892e0a672fdd9220dac83b8dbf2c658d2b
Grant Rutherford <contact.grantr@gmail.com>
2019-05-23 14:14:18 +0100

    MVP complete

--- src/main/java/com/codeclan/CourseBookingsLab/repositories/CustomerRepository/CustomerRepositoryImpl.java
+++ src/main/java/com/codeclan/CourseBookingsLab/repositories/CustomerRepository/CustomerRepositoryImpl.java
@@ -1,4 +1,41 @@
 package com.codeclan.CourseBookingsLab.repositories.CustomerRepository;
 
+import com.codeclan.CourseBookingsLab.models.Customer;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
 public class CustomerRepositoryImpl implements CustomerRepositoryCustom {
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Transactional
+    public List<Customer> findCustomersByCourse(Long id){
+        List<Customer> result = null;
+
+        Session session = entityManager.unwrap(Session.class);
+
+        try{
+            Criteria cr = session.createCriteria(Customer.class);
+            cr.createAlias("bookings", "bookingAlias");
+            cr.createAlias("bookingAlias.course", "courseAlias");
+            cr.add(Restrictions.eq("courseAlias.id", id));
+
+            result = cr.list();
+        }
+        catch(HibernateException ex){
+            ex.printStackTrace();
+        }
+        finally {
+            session.close();
+        }
+        return result;
+    }
 }
